973fd95317c105a8d90efc7f68d7b3641ec3bded
Harjyot <harjyot.codes@gmail.com>
2023-08-16 01:32:12 +0530

    first commit

---
+++ LandingPage/script.js
@@ -0,0 +1,20 @@
+gsap.to(".imageContainer", {
+    width: "100%",
+    ease: Expo.easeInOut,
+    duration: 2,
+    stagger: 2
+})
+
+gsap.to(".text h1", {
+    ease: Expo.easeInOut,
+    top: -50,
+    stagger: 2,
+    delay: 0.5
+})
+
+gsap.to(".text h1", {
+    ease: Expo.easeInOut,
+    top: "-400",
+    delay: 2.5,
+    stagger: 2
+})
